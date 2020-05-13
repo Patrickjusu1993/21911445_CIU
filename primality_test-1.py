696a14c1e02d9ac4c71c094913eed53be004d159
Patrickjusu1993 <64533173+Patrickjusu1993@users.noreply.github.com>
2020-05-13 15:15:44 +0000

    Add files via upload

---
+++ primality_test-1.py
@@ -0,0 +1,33 @@
+#a program that get input from user that show how many iteration needed to find 
+#out if a number is prime or not. If the number is not prime (composite) 
+#it gives the factor(s) of the given number.
+
+
+# Solution
+
+print("{Patrick Jusu 21911445}")
+
+number = int(input("Please input a number:"))
+
+ite = 3
+
+divisors = [i for i in range(2, number+1) if number % i == 0]
+it = number - 1
+
+if number > 1:
+    for i in range(2, number):
+        if (number % i) == 0:
+            print(number, "is composite number and factors are", divisors)
+            print("With 1st method number of iteration is", it)
+            print("With 2nd method number of iteration is", ite)
+            break
+    else:
+        print(number, "is prime number and factors are", divisors)
+        print("With 1st method number of iteration is", it)
+        print("With 2nd method number of iteration is", ite)
+
+else:
+    print(number, "is neither prime nor composite and factor is", divisors)
+
+    input("press enter to close")
+
